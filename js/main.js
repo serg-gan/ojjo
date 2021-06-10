4eb3459a1eb772ef5d2ff323218012be111adc7f
Sergiy <72192020+serg-gan@users.noreply.github.com>
2021-06-10 23:03:10 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// function ibg() {
+//     let ibg = document.querySelectorAll(".ibg");
+//     for (var i = 0; i < ibg.length; i++) {
+//         if (ibg[i].querySelector('img')) {
+//             ibg[i].style.backgroundImage = 'url(' + ibg[i].querySelector('img').getAttribute('src') + ')';
+//         }
+//     }
+// }
+// ibg();
+
+
+// $(document).ready(function () {
+//     $('.review__slider').slick({
+//         arrows: false,
+//         dots: true,
+//         loop: Infinity,
+//     });
+// });
